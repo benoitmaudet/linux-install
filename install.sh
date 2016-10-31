cd9ef3de6c5392005b26c87b7f4d0ba397f5e845
benoitmaudet <benoit.maudet@gmail.com>
2016-10-31 18:24:21 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+apt-get install ipcalc i3 gufw xinit terminator aha nmap fonts-font-awesome firefox wireshark openssl git sublime-text redshift nautilus openjdk-8-jdk fish john
+
+#Sublime-text
+  #firefox http://www.sublimetext.com/3
+
+#fish
+#curl -L http://get.oh-my.fish | fish
+#wget https://github.com/hbin/top-programming-fonts/raw/master/Menlo-Regular.ttf -P ~/.fonts/
+#omf install agnoster
